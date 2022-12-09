39254cbba18ad9a313f661222259968be1ca75bb
Chad Square <48771619+chadSquare@users.noreply.github.com>
2022-12-09 18:00:16 +0200

    Update scribeTrackerForTeams.js

--- scribeTrackerForTeams.js
+++ scribeTrackerForTeams.js
@@ -14,116 +14,102 @@
 // ==/UserScript==
 
  //  create buttons
-    const summaryBtn = document.createElement('button');
-    summaryBtn.innerText = 'Get Summary';
-    summaryBtn.setAttribute('id', 'summaryBtn');
-    summaryBtn.style.backgroundColor = "rgb(91, 95, 199)";
-    summaryBtn.style.color = "rgb(255, 255, 255)";
-    summaryBtn.style.borderRadius = "3px";
-    summaryBtn.style.border = "none";
-    summaryBtn.style.padding = "5px";
-    summaryBtn.style.marginLeft = "5px";
-    summaryBtn.style.marginRight = "5px";
-    summaryBtn.style.height = "32px";
+ const summaryBtn = document.createElement('button');
+ summaryBtn.innerText = 'Get Summary';
+ summaryBtn.setAttribute('id', 'summaryBtn');
+ summaryBtn.style.backgroundColor = "rgb(91, 95, 199)";
+ summaryBtn.style.color = "rgb(255, 255, 255)";
+ summaryBtn.style.borderRadius = "3px";
+ summaryBtn.style.border = "none";
+ summaryBtn.style.padding = "5px";
+ summaryBtn.style.marginLeft = "5px";
+ summaryBtn.style.marginRight = "5px";
+ summaryBtn.style.height = "32px";
 
-    const updateScribeBtn = document.createElement('button');
-    updateScribeBtn.innerText = 'Update Scribe';
-    updateScribeBtn.setAttribute('id', 'updateScribeBtn');
-    updateScribeBtn.style.backgroundColor = "rgb(91, 95, 199)";
-    updateScribeBtn.style.color = "rgb(255, 255, 255)";
-    updateScribeBtn.style.borderRadius = "3px";
-    updateScribeBtn.style.border = "none";
-    updateScribeBtn.style.padding = "5px";
-    updateScribeBtn.style.marginRight = "5px";
-    updateScribeBtn.style.height = "32px";
+ const updateScribeBtn = document.createElement('button');
+ updateScribeBtn.innerText = 'Update Scribe';
+ updateScribeBtn.setAttribute('id', 'updateScribeBtn');
+ updateScribeBtn.style.backgroundColor = "rgb(91, 95, 199)";
+ updateScribeBtn.style.color = "rgb(255, 255, 255)";
+ updateScribeBtn.style.borderRadius = "3px";
+ updateScribeBtn.style.border = "none";
+ updateScribeBtn.style.padding = "5px";
+ updateScribeBtn.style.marginRight = "5px";
+ updateScribeBtn.style.height = "32px";
 
-    // button hover effects
-    summaryBtn.addEventListener("mouseover", () => {
-        summaryBtn.style.backgroundColor = "rgb(68, 71, 145)";
-    });
-    summaryBtn.addEventListener("mouseleave", () => {
-        summaryBtn.style.backgroundColor = "rgb(91, 95, 199)";
-    });
-    updateScribeBtn.addEventListener("mouseover", () => {
-        updateScribeBtn.style.backgroundColor = "rgb(68, 71, 145)";
-    });
-    updateScribeBtn.addEventListener("mouseleave", () => {
-        updateScribeBtn.style.backgroundColor = "rgb(91, 95, 199)";
-    });
+ // button hover effects
+ summaryBtn.addEventListener("mouseover", () => {
+     summaryBtn.style.backgroundColor = "rgb(68, 71, 145)";
+ });
+ summaryBtn.addEventListener("mouseleave", () => {
+     summaryBtn.style.backgroundColor = "rgb(91, 95, 199)";
+ });
+ updateScribeBtn.addEventListener("mouseover", () => {
+     updateScribeBtn.style.backgroundColor = "rgb(68, 71, 145)";
+ });
+ updateScribeBtn.addEventListener("mouseleave", () => {
+     updateScribeBtn.style.backgroundColor = "rgb(91, 95, 199)";
+ });
 
 const teamMembers = [
-     'Bob',
-     'Sarah',
-     'Jeff',
-     'Kim',
-     'Lance',
-     'Jane',
-     'Paul',
-     'Maria'
-    ];
+ 'Liubomyr',
+ 'Jaco',
+ 'Nazar',
+ 'Hendri',
+ 'Waseem',
+ 'Marindi',
+ 'Chad',
+ 'Bala'
+ ];
 
 //  methods for script
 function getCurrentScribe() {
-    return localStorage.getItem('currentScribe');
+ return localStorage.getItem('currentScribe');
 }
 
 function getNextScribe() {
-    const currentScribe = getCurrentScribe();
-    const currentScribeIndex = teamMembers.indexOf(currentScribe);
-    if (currentScribeIndex >= teamMembers.length - 1) {
-        return teamMembers[0];
-    }
-    return teamMembers[currentScribeIndex + 1];
+ const currentScribe = getCurrentScribe();
+ const currentScribeIndex = teamMembers.indexOf(currentScribe);
+ if (currentScribeIndex >= teamMembers.length - 1) {
+     return teamMembers[0];
+ }
+ return teamMembers[currentScribeIndex + 1];
 }
 
 function setNextScribe() {
-    const newPerson = getNextScribe();
-    localStorage.setItem('currentScribe', newPerson);
-    localStorage.setItem('lastScribeUpdate', new Date);
+ const newPerson = getNextScribe();
+ localStorage.setItem('currentScribe', newPerson);
+ localStorage.setItem('lastScribeUpdate', new Date);
 }
 
 function getLastUpdateDate() {
-    return localStorage.getItem('lastScribeUpdate');
+ return localStorage.getItem('lastScribeUpdate');
 }
 
-function getSummary() {
-    alert(`-The last Scribe was: ${getCurrentScribe()}, \n-The next Scribe should be: ${getNextScribe()}, \n-The last update was: ${getLastUpdateDate()}`);
+function getSummary(e) {
+ if(e !== 'remove') {
+     e.preventDefault();
+ }
+ alert(`-The last Scribe was: ${getCurrentScribe()}, \n-The next Scribe should be: ${getNextScribe()}, \n-The last update was: ${getLastUpdateDate()}`);
 }
 
 function runUpdate() {
-    setNextScribe();
-    alert(`-The new Scribe is: ${getCurrentScribe()}`);
+ setNextScribe();
+ alert(`-The new Scribe is: ${getCurrentScribe()}`);
 }
 
 
 
 (function() {
-    'use strict';
+ 'use strict';
+ waitForKeyElements (".power-bar-margin", () => {
+     const powerBarMargin = document.querySelector(".power-bar-margin");
+     console.log(powerBarMargin);
+     powerBarMargin.insertAdjacentElement('afterend', summaryBtn)
+     summaryBtn.insertAdjacentElement('afterend', updateScribeBtn)
 
-    console.log('running scribe script before wait');
-      waitForKeyElements ("*[data-tid='new-meeting-split-button']", () => {
-          console.log('running scribe script after wait');
-          const newMeetingBtn = document.querySelector("[data-tid='new-meeting-split-button']");
-          console.log(newMeetingBtn);
-
-//         add buttons to teams top-bar (next to profile icon)
-          const teamsTopBar = document.querySelector('.ui-flex.cm.dp.cq.dm.dq.dr');
-          console.log(teamsTopBar);
-          if (!document.querySelector("#summaryBtn") && !document.querySelector("#updateScribeBtn")) {
-              teamsTopBar.append(summaryBtn);
-              teamsTopBar.append(updateScribeBtn);
-
-              //  add listeners to buttons
-              document.querySelector("#summaryBtn").addEventListener("click", () => {
-                  getSummary();
-              });
-
-              document.querySelector("#updateScribeBtn").addEventListener("click", () => {
-                  runUpdate();
-              });
-          }
-
-
-      });
+     document.querySelector("#summaryBtn").addEventListener("click", (e) => getSummary(e));
+     document.querySelector("#updateScribeBtn").addEventListener("click", () => runUpdate());
+   });
 
 })();
